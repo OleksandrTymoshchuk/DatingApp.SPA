d3228d56290637c89fb34ba653e8db9ae7e6e18c
Oleksandr Tymoshchuk <oleksandrtymoshchuk22@gmail.com>
2019-12-22 00:24:25 +0200

    Created user service and implemented method for getting all users in memberList component

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { User } from './../models/User';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -8,7 +9,7 @@ import {JwtHelperService} from '@auth0/angular-jwt';
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl = 'https://localhost:5001/auth/';
+  baseUrl = environment.apiUrl + 'auth/';
   jwtHelper = new JwtHelperService();
   decodedToken: any;
 
