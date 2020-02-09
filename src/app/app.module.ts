c1f4aedc68b677d7391067543ee1aec7537da977
Oleksandr Tymoshchuk <oleksandrtymoshchuk22@gmail.com>
2020-02-09 21:27:23 +0200

    Added MessageMember component for displaying message thread

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 import { MessagesResolver } from './_resolvers/messages.relosver';
 import { ListsResolver } from './_resolvers/lists.resolver';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
@@ -60,6 +61,7 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       MessagesComponent,
       MemberDetailComponent,
       MemberEditComponent,
+      MemberMessagesComponent,
       PhotoEditorComponent,
       TimeAgoPipe
    ],
