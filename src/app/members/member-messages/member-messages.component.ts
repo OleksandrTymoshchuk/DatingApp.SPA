6ee9516d844ee678014d8b0a6e657d2f0b643007
Oleksandr Tymoshchuk <oleksandrtymoshchuk22@gmail.com>
2020-02-11 00:02:11 +0200

    Added the Mark as Read functionality

--- src/app/members/member-messages/member-messages.component.ts
+++ src/app/members/member-messages/member-messages.component.ts
@@ -4,6 +4,7 @@ import { UserService } from './../../_services/user.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Message } from 'src/app/_models/message';
 import { error } from 'protractor';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-messages',
@@ -23,7 +24,16 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages() {
+    const currentUserId = +this.authService.decodedToken.nameid;
     this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+    .pipe(
+      tap(messages => {
+        for (let i = 0; i < messages.length; i++) {
+          if (messages[i].isRead === false && messages[i].recipientId === currentUserId)
+            this.userService.markAsRead(currentUserId, messages[i].id);
+        }
+      })
+    )
       .subscribe(messages => {
         this.messages = messages;
       }, error => {
