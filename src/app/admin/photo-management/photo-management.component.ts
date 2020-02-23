ee1897583810d9c8a8745dc354fc79b77a330c23
Oleksandr Tymoshchuk <oleksandrtymoshchuk22@gmail.com>
2020-02-23 20:02:24 +0200

    Created the Photo Management system on the Admin page

--- src/app/admin/photo-management/photo-management.component.ts
+++ src/app/admin/photo-management/photo-management.component.ts
@@ -1,4 +1,6 @@
+import { AdminService } from './../../_services/admin.service';
 import { Component, OnInit } from '@angular/core';
+import { error } from 'protractor';
 
 @Component({
   selector: 'app-photo-management',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-management.component.css']
 })
 export class PhotoManagementComponent implements OnInit {
+  photos: any;
 
-  constructor() { }
+  constructor(private adminService: AdminService) { }
 
   ngOnInit() {
+    this.getPhotosForApproval();
   }
 
+  getPhotosForApproval() {
+    this.adminService.getPhotosForApproval().subscribe((photos) => {
+      this.photos = photos;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  approvePhoto(photoId) {
+    this.adminService.approvePhoto(photoId).subscribe(() => {
+      this.photos.splice(this.photos.findIndex(p => p.id === photoId), 1);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  rejectPhoto(photoId) {
+    this.adminService.rejectPhoto(photoId).subscribe(() => {
+      this.photos.splice(this.photos.findIndex(p => p.id === photoId), 1);
+    }, error => {
+      console.log(error);
+    });
+  }
 }
